de45d2994fd9a47fed30edec45d0885e471ee106
Timothy Ryon <tim@timryon.com>
2024-03-26 17:18:25 -0400

    finish lab and tests pass but with extra code

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,63 @@
 #!/usr/bin/env python3
+# value = "Is this a value?"
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self.value.endswith(".")
+  # above line is more pythonic and can replace the 4 below
+  # if self.value.endswith(".") is True: 
+    #   return True
+    # else:
+    #   return False
+      
+  def is_question(self):
+    return self.value.endswith("?")
+
+  def is_exclamation(self):
+    return self.value.endswith("!")
+
+  # def count_sentences(self):
+  #   value = self.value
+  #   for punc in ['!','?']:
+  #       value = value.replace(punc, '.')
+    
+  #   sentences = [s for s in value.split('.') if s]
+    
+  #   return len(sentences)
+
+
+  def count_sentences(self):
+    endings = ("!","?")
+    # return len(self.value.split()) only works for one word sentences!
+    # if self.value == "":
+    #   return 0
+    # else:
+    for end in endings:
+      self.value = self.value.replace(end,".")
+    change_ending = self.value.split(".")
+    print(change_ending)
+    for change in change_ending:
+      print(change)
+      if change == "":
+        change_ending.remove(change)
+        print(change_ending)
+    # breakpoint()   
+    for change in change_ending:
+      if change_ending[-1] == "":
+        change_ending.pop() 
+    return len(change_ending)
+    # return len(self.value.split("."))
